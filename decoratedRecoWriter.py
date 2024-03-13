8df322f17628b3cc3299cb5a363590b6590ddec6
Anouar Achghaf <achghafanouar@gmail.com>
2024-03-13 13:27:14 +0100

    switch to postgres

--- decoratedRecoWriter.py
+++ decoratedRecoWriter.py
@@ -3,23 +3,17 @@ from datetime import datetime
 from confluent_kafka import Consumer, KafkaError
 import json
 import os
-import boto3
+import psycopg2
 
-AWS_REGION = os.getenv("AWS_REGION", "eu-west-1")
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-TS_DATABASE = os.getenv("TS_DATABASE_NAME", "flightdb")
-TS_TABLE = os.getenv("TS_TABLE_NAME", "flight-recos")
+PG_HOST = os.getenv("PG_HOST", "localhost")
+PG_PORT = os.getenv("PG_PORT", "5432")
+PG_DATABASE = os.getenv("PG_DATABASE", "flightdb")
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_TABLE = os.getenv("PG_TABLE", "flight_recos")
 
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "decorated-recos")
-
-timestream_client = boto3.client(
-    "timestream-write",
-    region_name=AWS_REGION,
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-)
 
 consumer_conf = {
     "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
@@ -30,8 +24,37 @@ consumer = Consumer(consumer_conf)
 consumer.subscribe([KAFKA_TOPIC])
 
 
-def populate_timestream_from_kafka():
+def create_table(cursor):
+    create_table_sql = f"""
+        CREATE TABLE IF NOT EXISTS {PG_TABLE} (
+            search_id VARCHAR,
+            search_country VARCHAR,
+            OnD VARCHAR,
+            trip_type VARCHAR,
+            main_airline VARCHAR,
+            price_EUR FLOAT,
+            advance_purchase INTEGER,
+            number_of_flights INTEGER,
+            time TIMESTAMP
+        )
+    """
+    cursor.execute(create_table_sql)
+
+
+def populate_postgres_from_kafka():
     try:
+        print("Consumer started")
+        conn = psycopg2.connect(
+            host=PG_HOST,
+            port=PG_PORT,
+            dbname=PG_DATABASE,
+            user=PG_USER,
+            password=PG_PASSWORD,
+        )
+        cursor = conn.cursor()
+
+        create_table(cursor)
+
         while True:
             msg = consumer.poll(timeout=1.0)
             if msg is None:
@@ -59,48 +82,38 @@ def populate_timestream_from_kafka():
             )
 
             for reco in json_data["recos"]:
-                dimensions = [
-                    {"Name": "search_id", "Value": search_id},
-                    {"Name": "search_country", "Value": search_country},
-                    {"Name": "OnD", "Value": OnD},
-                    {"Name": "trip_type", "Value": trip_type},
-                    {
-                        "Name": "main_airline",
-                        "Value": reco["main_marketing_airline"],
-                    },
-                ]
-                measures = [
-                    {"Name": "price_EUR", "Value": str(reco["price_EUR"])},
-                    {
-                        "Name": "advance_purchase",
-                        "Value": str(json_data["advance_purchase"]),
-                    },
-                    {
-                        "Name": "number_of_flights",
-                        "Value": str(reco["nb_of_flights"]),
-                    },
-                ]
-
-                timestream_client.write_records(
-                    DatabaseName=TS_DATABASE,
-                    TableName=TS_TABLE,
-                    Records=[
-                        {
-                            "Dimensions": dimensions,
-                            "MeasureName": measure["Name"],
-                            "MeasureValue": measure["Value"],
-                            "Time": str(int(timestamp.timestamp() * 1000)),
-                        }
-                        for measure in measures
-                    ],
+                sql = f"""
+                    INSERT INTO {PG_TABLE} (search_id, search_country, OnD, trip_type, main_airline,
+                                            price_EUR, advance_purchase, number_of_flights, time)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """
+                cursor.execute(
+                    sql,
+                    (
+                        search_id,
+                        search_country,
+                        OnD,
+                        trip_type,
+                        reco["main_marketing_airline"],
+                        reco["price_EUR"],
+                        json_data["advance_purchase"],
+                        reco["nb_of_flights"],
+                        timestamp,
+                    ),
                 )
+
+                conn.commit()
 
     except KeyboardInterrupt:
         consumer.close()
+        cursor.close()
+        conn.close()
 
     finally:
         consumer.close()
+        cursor.close()
+        conn.close()
 
 
 if __name__ == "__main__":
-    populate_timestream_from_kafka()
+    populate_postgres_from_kafka()
